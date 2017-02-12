d839f321003f9cbd3bca5d6ca61e466bc1c45ea6
JiaoYuhang <812377857@qq.com>
2017-02-12 21:23:17 +0800

    Update Using_jieba.py

--- Using_jieba.py
+++ Using_jieba.py
@@ -1,9 +1,8 @@
+#-*-coding:utf-8-*- 
 import jieba
 from multiprocessing import Pool,cpu_count
 import numpy as np
-
-StopWord
-
+import codecs
 
 def wordcont(wordlist):
 	dic = {}
@@ -24,11 +23,20 @@ def cut(sentence):
 		return [i for i in sentence if i not in StopWord]
 
 if __name__ == '__main__':
+	# 读取StopWord.txt获取停用词,标准格式为 word + '\r\n'
+	f = codecs.open('StopWord.txt','r','utf8')
+	StopWord  = f.read().split('\r\n')
+	f.close()
+	# 去除最后一个'\r\n' 
+	StopWord.remove(StopWord[-1])
+	
 	path = raw_input("Enter the path: ")
 	data = np.load(path)
-	pool = Pool(cpu_count()-1)
+	# 使用多进程编程提高分词速度  
+	pool = Pool(cpu_count())
 	data = pool.map(cut, data)
 	pool.close()
 	pool.join()	
 	data = np.array(data)
 	np.save(path,data)
+	
