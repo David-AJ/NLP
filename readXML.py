12fb47cc663bd83a2335b58584fb311468f96faf
JiaoYuhang <812377857@qq.com>
2017-02-12 21:14:16 +0800

    Update readXML.py

--- readXML.py
+++ readXML.py
@@ -1,20 +1,22 @@
 import xml.etree.ElementTree as ET
 import numpy as np
 
-f = open('news_tensite_xml.dat','r')
+# data.xml代表需要读取的xml文件,本例中XML包含节点名为'url' 及 'content'
+f = open('data.xml','r')
 t = f.readlines()
 f.close()
 
+
 urls = []
 docs = []
-for i in range(len(t)/6):
-	try:
-		tree = ET.fromstringlist(t[i*6:(i+1)*6])
-		urls.append(tree.find('url').text)
-		docs.append(tree.find('content').text)
-	except ET.ParseError:
-		urls.append(t[i*6+1][5:-7])
-		docs.append(t[i*6+4][9:-11])
+
+try:
+	tree = ET.fromstringlist(t)
+	urls.append(tree.find('url').text)
+	docs.append(tree.find('content').text)
+except ET.ParseError:
+	exceptcode
+	
 np.save('urls',np.array(urls))
 np.save('docs',np.array(docs))
 
