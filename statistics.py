aa093344e5969071a01f56308594fa1f1adfe3f1
JiaoYuhang <812377857@qq.com>
2017-02-12 21:25:01 +0800

    add word_frequency
    统计词频

---
+++ statistics.py
@@ -0,0 +1,14 @@
+﻿#-*-coding:utf-8-*- 
+def wordcont(wordlist):
+	dic = {}
+	for i in wordlist[:,1]:
+		if i == None:
+			continue
+		for j in i:
+			if j in dic:
+				dic[j] = dic[j] + 1
+			else:
+				dic[j] = 1
+	return sorted(dic.iteritems(),key=lambda a:a[1],reverse=True)
+
+	
